306e3b3ebf9a94f8e6baf90e13a6765469105440
Diego-Alexis-Leon <0264410@up.edu.mx>
2024-10-19 02:10:44 -0600

    image

--- index.js
+++ index.js
@@ -7,11 +7,21 @@ app.set('view engine', 'ejs');
 // Definir la carpeta donde estarán las vistas
 app.set('views', './views');
 
+app.use(express.static('Public'));
+
+const pokedex = [
+  { id: 1, name: "Bulbasaur", type: "Grass/Poison", image:""},
+  { id: 2, name: "Ivysaur", type: "Grass/Poison", image:"Public\images\Ivysaur.png"},
+  { id: 3, name: "Venusaur", type: "Grass/Poison",image:"Public\images\Venusaur.jpeg"},
+  // Agrega más Pokémon
+];
+
 // Ruta principal que renderiza la plantilla EJS
 app.get('/', (req, res) => {
-  res.render('index', { titulo: 'Pokedex' });
+  res.render('index', { titulo: 'Pokedex' , pokemons: pokedex });
 });
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
+
